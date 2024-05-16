cea41df0ae3ce96872268788617b334874eeb684
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-05-16 00:02:13 -0600

    Agregando el tema de funciones

---
+++ funciones.js
@@ -0,0 +1,9 @@
+// Declaración de función (Puedes usarlas antes de declararlas)
+function cuadrado(numero){
+    return numero * numero;
+}
+
+//Expresión de función
+//let func = function saludar(){console.log("Hola");}
+
+console.log(cuadrado(3));
